46164683156e4cd23cfd597db9664506c7e53e72
Sarthak <sarthakgargx@gmail.com>
2021-10-01 11:27:29 +0530

    final-commit

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-console.log("Name : Sarthak Garg")
-console.log("Age : 19")
-console.log("City : Vidisha")
-console.log("State : Madhya Pradesh")
-console.log("Country : India")
-console.log("Gender : Male")
-console.log("Development")
-console.log("We are Adding this in main branch")
+console.log("we are using this code for development branch")
 

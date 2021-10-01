8ecb45c3df6e1977225077b93dd5d154feec60a4
Sarthak <sarthakgargx@gmail.com>
2021-10-01 12:48:34 +0530

    Development Code

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-console.log("We are using this code for development branch")
-console.log("here we are changing codes")
-=======
-console.log("This is the code of new-feature branch")
-
->>>>>>> 0f56ec60513dbfc7a3186ddcf35348c6fa83d285
-
+console.log ("This is Development Branch")

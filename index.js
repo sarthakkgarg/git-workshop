3f2e40dde03bb1e6fa8a9ce87cdee9953e4fecf0
Sarthak <sarthakgargx@gmail.com>
2021-10-01 10:29:43 +0530

    updated code

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-console.log("Name : Sarthak Garg")
-console.log("Age : 19")
-console.log("City : Vidisha")
-console.log("State : Madhya Pradesh")
-console.log("Country : India")
-console.log("Gender : Male")
-console.log("Development")
-console.log("We are Adding this in main branch")
+console.log(" We are using this code for new-feature")
+
 
